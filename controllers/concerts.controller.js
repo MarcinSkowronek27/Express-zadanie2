0091cc02a6f180210019209dfdb958e4d228b87d
Marcin Skowronek <marcinfoto35@op.pl>
2021-11-06 17:50:20 +0100

    Some correction of sanitize function

--- controllers/concerts.controller.js
+++ controllers/concerts.controller.js
@@ -79,11 +79,11 @@ exports.getId = async (req, res) => {
 
 exports.post = async (req, res) => {
   const { performer, genre, price, day, image, id } = req.body;
-  clean = sanitize(performer);
-
+  clean = sanitize(req.body.performer);
+  console.log('clean:', clean);
   try {
     const con = await Concert.findOne({ performer: clean });
-    // console.log('con:', con);
+    console.log('con:', con);
     if (!con) {
       // res.status(400).send('The slot is already taken...').json( { message: "The slot is already taken..." });
       const newConcert = new Concert({ performer, genre, price, day, image, id });
